06d70b75f9e55503046afe52f996a48bff94bf9c
Brandon Dewitt <brandonsdewitt@gmail.com>
2012-06-13 13:31:56 -0600

    add to ActiveRemote module

--- lib/active_remote/protobuf_pagination.rb
+++ lib/active_remote/protobuf_pagination.rb
@@ -1,64 +1,65 @@
 require 'active_remote/protobuf_helpers'
 
-module ProtobufPagination
+module ActiveRemote
+  module ProtobufPagination
 
-  def self.included(klass)
-    klass.__send__(:include, ::ActiveRemote::ProtobufHelpers)
-    klass.extend(::ProtobufPagination)
-  end
+    def self.included(klass)
+      klass.__send__(:include, ::ActiveRemote::ProtobufHelpers)
+      klass.extend(::ProtobufPagination)
+    end
 
-  def pagination_response_options(request, search_relation)
-    options = nil
-    if _protobuf_paginate_ready?(request)
-      options = Atlas::SearchOptions.new({
-        :pagination => Atlas::Pagination.new({
-          :total_entries => search_relation.total_entries,
-          :total_pages => search_relation.total_pages,
-          :count => search_relation.count
+    def pagination_response_options(request, search_relation)
+      options = nil
+      if _protobuf_paginate_ready?(request)
+        options = Atlas::SearchOptions.new({
+          :pagination => Atlas::Pagination.new({
+            :total_entries => search_relation.total_entries,
+            :total_pages => search_relation.total_pages,
+            :count => search_relation.count
+          })
         })
-      })
-    end
-
-    return options
-  end
-
-  def pagination_scope(search_scope, proto_object)
-    if _protobuf_paginate_ready?(proto_object)
-      pagination = proto_object.options.pagination
-      search_scope = search_scope.paginate(:page => pagination.page, :per_page => pagination.per_page)
-      search_scope = _protobuf_paginate_order(search_scope, pagination)
-    end
-
-    return search_scope
-  end
-
-  private 
-
-  def _protobuf_paginate_order(search_scope, pagination)
-    if respond_to_and_has_and_present?(pagination, :sort_column)
-      order_by = pagination.sort_column
-
-      if _protobuf_paginate_order_desc?(pagination)
-        order_by += ' DESC'
       end
 
-      return search_scope.order(order_by)
+      return options
     end
 
-    return search_scope
-  end
+    def pagination_scope(search_scope, proto_object)
+      if _protobuf_paginate_ready?(proto_object)
+        pagination = proto_object.options.pagination
+        search_scope = search_scope.paginate(:page => pagination.page, :per_page => pagination.per_page)
+        search_scope = _protobuf_paginate_order(search_scope, pagination)
+      end
 
-  def _protobuf_paginate_order_desc?(pagination)
-    respond_to_and_has_and_present?(pagination, :sort_direction) && 
-      pagination.sort_direction == ::Atlas::DBDirection::DESC
-  end
+      return search_scope
+    end
 
-  def _protobuf_paginate_ready?(proto)
-    respond_to_and_has_and_present?(proto, :options) &&
-      respond_to_and_has_and_present?(proto.options, :pagination) &&
-      respond_to_and_has_and_present?(proto.options.pagination, :per_page) &&
-      respond_to_and_has_and_present?(proto.options.pagination, :page) 
-  end
+    private 
 
+    def _protobuf_paginate_order(search_scope, pagination)
+      if respond_to_and_has_and_present?(pagination, :sort_column)
+        order_by = pagination.sort_column
+
+        if _protobuf_paginate_order_desc?(pagination)
+          order_by += ' DESC'
+        end
+
+        return search_scope.order(order_by)
+      end
+
+      return search_scope
+    end
+
+    def _protobuf_paginate_order_desc?(pagination)
+      respond_to_and_has_and_present?(pagination, :sort_direction) && 
+        pagination.sort_direction == ::Atlas::DBDirection::DESC
+    end
+
+    def _protobuf_paginate_ready?(proto)
+      respond_to_and_has_and_present?(proto, :options) &&
+        respond_to_and_has_and_present?(proto.options, :pagination) &&
+        respond_to_and_has_and_present?(proto.options.pagination, :per_page) &&
+        respond_to_and_has_and_present?(proto.options.pagination, :page) 
+    end
+
+  end
 end
-
