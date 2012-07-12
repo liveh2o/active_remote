8274fbd7d3f057523bb1c868f1f9fbd580490683
Sterling Cobb <sterlingcobb@gmail.com>
2012-07-12 11:32:31 -0600

    Wrap instance and class methods in a module.
    Persistence methods are now being added correctly
    to the base class.

--- lib/active_remote/persistence.rb
+++ lib/active_remote/persistence.rb
@@ -1,221 +1,220 @@
 module ActiveRemote
   module Persistence
 
+    def self.included(klass)
+      klass.class_eval do 
+        extend ActiveRemote::Persistence::ClassMethods
+        include ActiveRemote::Persistence::InstanceMethods
+      end
 
-    ##
-    # Class methods
-    #
-
-    def self.search(args, options = {})
-      remote = self.new
-      remote._active_remote_search(args)
-      options.fetch(:records, true) ? remote.serialize_records : remote
-    end
-
-    def self.search!(request, options = {})
-      remote = self.new
-      remote._active_remote_search!(request)
-      options.fetch(:records, true) ? remote.__send__(:records) : remote
-    end
-
-    def self.save(attributes)
-      remote = self.new(attributes)
-      remote.save
-      remote
-    end
-
-
-    ##
-    # Bulk class methods
-    #
-    def self.create_all(*records)
-      options = records.extract_options!
-      remote = self.new
-      remote.execute(:create_all, records.flatten, :bulk => true)
-      options.fetch(:records, true) ? remote.__send__(:records) : remote
-    end
-
-    def self.create_all!(request, options = {})
-      remote = self.new
-      remote.execute!(:create_all, request)
-      options.fetch(:records, true) ? remote.__send__(:records) : remote
-    end
-
-    def self.update_all(*records)
-      options = records.extract_options!
-      remote = self.new
-      remote.execute(:update_all, records.flatten, :bulk => true)
-      options.fetch(:records, true) ? remote.__send__(:records) : remote
-    end
-
-    def self.update_all!(request, options = {})
-      remote = self.new
-      remote.execute!(:update_all, request)
-      options.fetch(:records, true) ? remote.__send__(:records) : remote
-    end
-
-    def self.delete_all(*records)
-      options = records.extract_options!
-      remote = self.new
-      remote.execute(:delete_all, records.flatten, :bulk => true)
-      options.fetch(:records, true) ? remote.__send__(:records) : remote
-    end
-
-    def self.delete_all!(request, options = {})
-      remote = self.new
-      remote.execute!(:delete_all, request)
-      options.fetch(:records, true) ? remote.__send__(:records) : remote
-    end
-
-    def self.destroy_all(*records)
-      options = records.extract_options!
-      remote = self.new
-      remote.execute(:destroy_all, records.flatten, :bulk => true)
-      options.fetch(:records, true) ? remote.__send__(:records) : remote
-    end
-
-    def self.destroy_all!(request, options = {})
-      remote = self.new
-      remote.execute!(:destroy_all, request)
-      options.fetch(:records, true) ? remote.__send__(:records) : remote
-    end
-
-    def self.request(rpc_method, args, options = {})
-      bulk = options.fetch(:bulk, false)
-      record_or_records = bulk ? records_hash(rpc_method, args) : args
-      message_class = request_type(rpc_method)
-      build_message(message_class, record_or_records)
-    end
-
-    def self.default_message
-      Atlas.const_get(service_name).const_get(self.name.demodulize)
-    end
-
-
-    ##
-    # Class aliases
-    #
-    class << self
-      alias_method :find, :search
-      alias_method :find!, :search!
-      alias_method :create, :save
-      alias_method :update, :save
-    end
-
-
-    ##
-    # Instance methods
-    #
-    def _active_remote_search(args)
-      run_callbacks :search do
-        execute(:search, args)
+      class << klass
+        alias_method :find, :search
+        alias_method :find!, :search!
+        alias_method :create, :save
+        alias_method :update, :save
       end
     end
 
-    def _active_remote_search!(request)
-      run_callbacks :search do
-        execute!(:search, request)
+    module ClassMethods
+      def search(args, options = {})
+        remote = self.new
+        remote._active_remote_search(args)
+        options.fetch(:records, true) ? remote.serialize_records : remote
+      end
+
+      def search!(request, options = {})
+        remote = self.new
+        remote._active_remote_search!(request)
+        options.fetch(:records, true) ? remote.__send__(:records) : remote
+      end
+
+      def save(attributes)
+        remote = self.new(attributes)
+        remote.save
+        remote
+      end
+
+
+      ##
+      # Bulk class methods
+      #
+      def create_all(*records)
+        options = records.extract_options!
+        remote = self.new
+        remote.execute(:create_all, records.flatten, :bulk => true)
+        options.fetch(:records, true) ? remote.__send__(:records) : remote
+      end
+
+      def create_all!(request, options = {})
+        remote = self.new
+        remote.execute!(:create_all, request)
+        options.fetch(:records, true) ? remote.__send__(:records) : remote
+      end
+
+      def update_all(*records)
+        options = records.extract_options!
+        remote = self.new
+        remote.execute(:update_all, records.flatten, :bulk => true)
+        options.fetch(:records, true) ? remote.__send__(:records) : remote
+      end
+
+      def update_all!(request, options = {})
+        remote = self.new
+        remote.execute!(:update_all, request)
+        options.fetch(:records, true) ? remote.__send__(:records) : remote
+      end
+
+      def delete_all(*records)
+        options = records.extract_options!
+        remote = self.new
+        remote.execute(:delete_all, records.flatten, :bulk => true)
+        options.fetch(:records, true) ? remote.__send__(:records) : remote
+      end
+
+      def delete_all!(request, options = {})
+        remote = self.new
+        remote.execute!(:delete_all, request)
+        options.fetch(:records, true) ? remote.__send__(:records) : remote
+      end
+
+      def destroy_all(*records)
+        options = records.extract_options!
+        remote = self.new
+        remote.execute(:destroy_all, records.flatten, :bulk => true)
+        options.fetch(:records, true) ? remote.__send__(:records) : remote
+      end
+
+      def destroy_all!(request, options = {})
+        remote = self.new
+        remote.execute!(:destroy_all, request)
+        options.fetch(:records, true) ? remote.__send__(:records) : remote
+      end
+
+      def request(rpc_method, args, options = {})
+        bulk = options.fetch(:bulk, false)
+        record_or_records = bulk ? records_hash(rpc_method, args) : args
+        message_class = request_type(rpc_method)
+        build_message(message_class, record_or_records)
+      end
+
+      def default_message
+        Atlas.const_get(service_name).const_get(self.name.demodulize)
+      end
+
+      private
+
+      def records_hash(rpc_method, records)
+        message = request_type(rpc_method).new
+        field = message.fields.values.first.name
+        { field => records.flatten.compact.uniq }
+      end
+
+      def request_type(rpc_method)
+        service_class.rpcs[service_class][rpc_method].request_type
+      end
+
+      def response_type(rpc_method)
+        service_class.rpcs[service_class][rpc_method].response_type
       end
     end
 
-    def assign_attributes(attributes)
-      self.attributes.merge!(attributes)
-    end
-
-    def delete
-      execute(:delete, attributes)
-    end
-
-    def destroy
-      execute(:destroy, attributes)
-    end
-
-    def execute(rpc_method, args, options = {})
-      execute!(rpc_method, request(rpc_method, args, options))
-    end
-
-    def execute!(rpc_method, request)
-      @last_request = request
-
-      rpc_service_client.__send__(rpc_method, @last_request) do |c|
-        c.on_failure do |error|
-          errors << error.message
-          @last_response = error
-        end
-
-        c.on_success do |response|
-          @last_response = response
-
-          # TODO: this should be consolidated so its not repeated in the constructor
-          @attributes = HashWithIndifferentAccess.new(response.to_hash)
-          mimic_response(response)
+    module InstanceMethods
+      def _active_remote_search(args)
+        run_callbacks :search do
+          execute(:search, args)
         end
       end
 
-      return success?
-    end
+      def _active_remote_search!(request)
+        run_callbacks :search do
+          execute!(:search, request)
+        end
+      end
 
-    def has_errors?
-      return errors.length > 0
-    end
+      def assign_attributes(attributes)
+        self.attributes.merge!(attributes)
+      end
 
-    def new_record?
-      attributes[:guid].nil?
-    end
+      def delete
+        execute(:delete, attributes)
+      end
 
-    def records
-      raise last_response.message if has_errors?
+      def destroy
+        execute(:destroy, attributes)
+      end
 
-      last_response.records
-    end
+      def execute(rpc_method, args, options = {})
+        execute!(rpc_method, request(rpc_method, args, options))
+      end
 
-    def request(rpc_method, args, options = {})
-      self.class.request(rpc_method, args, options)
-    end
+      def execute!(rpc_method, request)
+        @last_request = request
 
-    def save
-      run_callbacks :save do
-        if attributes.fetch(:id, false) || attributes.fetch(:guid, false)
-          execute(:update, attributes)
-        else
-          execute(:create, attributes)
+        rpc_service_client.__send__(rpc_method, @last_request) do |c|
+          c.on_failure do |error|
+            errors << error.message
+            @last_response = error
+          end
+
+          c.on_success do |response|
+            @last_response = response
+
+            # TODO: this should be consolidated so its not repeated in the constructor
+            @attributes = HashWithIndifferentAccess.new(response.to_hash)
+            mimic_response(response)
+          end
         end
 
-        success?
+        return success?
       end
-    end
 
-    def success?
-      !has_errors?
-    end
+      def has_errors?
+        return errors.length > 0
+      end
 
-    def update_attributes(attributes)
-      assign_attributes(attributes)
-      save
-    end
+      def new_record?
+        attributes[:guid].nil?
+      end
 
-    private
+      def records
+        raise last_response.message if has_errors?
 
-    def self.records_hash(rpc_method, records)
-      message = request_type(rpc_method).new
-      field = message.fields.values.first.name
-      { field => records.flatten.compact.uniq }
-    end
+        last_response.records
+      end
 
-    def self.request_type(rpc_method)
-      service_class.rpcs[service_class][rpc_method].request_type
-    end
+      def request(rpc_method, args, options = {})
+        self.class.request(rpc_method, args, options)
+      end
 
-    def self.response_type(rpc_method)
-      service_class.rpcs[service_class][rpc_method].response_type
-    end
+      def save
+        run_callbacks :save do
+          if attributes.fetch(:id, false) || attributes.fetch(:guid, false)
+            execute(:update, attributes)
+          else
+            execute(:create, attributes)
+          end
 
-    def rpc_service_client
-      @rpc_service_client ||= self.class.service_class.client
-    end
+          success?
+        end
+      end
 
-    def records_hash(rpc_method, records)
-      self.class.records_hash(rpc_method, records)
+      def success?
+        !has_errors?
+      end
+
+      def update_attributes(attributes)
+        assign_attributes(attributes)
+        save
+      end
+
+
+      def rpc_service_client
+        @rpc_service_client ||= self.class.service_class.client
+      end
+
+      def records_hash(rpc_method, records)
+        self.class.records_hash(rpc_method, records)
+      end
     end
 
   end
