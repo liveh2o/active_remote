235b890491436250642cceef1212112d44f74c04
Adam Hutchison <liveh2o@gmail.com>
2012-09-10 03:06:56 -0500

    Stubbed out the DSL module specs.

--- spec/lib/active_remote/dsl_spec.rb
+++ spec/lib/active_remote/dsl_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
 describe ActiveRemote::DSL do
-  pending "Write specs for this!"
+  describe ".app" do
+    context "when given a value" do
+      it "sets @app to the value"
+    end
+  end
+
+  describe ".attr_publishable" do
+    it "appends given attributes to @publishable_attributes"
+  end
+
+  describe ".namespace" do
+    context "when given a value" do
+      it "sets @namespace to the value"
+    end
+  end
+
+  describe ".service" do
+    context "when nil" do
+      it "raises an exception"
+    end
+
+    context "when given a value" do
+      it "sets @service to the value"
+    end
+  end
+
+  describe ".service_class" do
+    context "when nil" do
+      it "determines the service class"
+    end
+
+    context "when given a value" do
+      it "sets @service_class to the value"
+    end
+  end
 end
