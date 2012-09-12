ce0df2cdc454a43569ecf77c1fdbb59cd7875249
Adam Hutchison <liveh2o@gmail.com>
2012-09-12 17:02:18 -0600

    No more mimicing!

--- lib/active_remote/serialization.rb
+++ lib/active_remote/serialization.rb
@@ -9,30 +9,10 @@ module ActiveRemote
       end
     end
 
-    # Returns an array of records that have been endowed with
-    # ActiveRemote capabilities via #mimic_response.
-    #
     def serialize_records
       last_response.records.map do |record|
         remote = self.class.new(record.to_hash)
-        remote.__send__(:mimic_response, record)
         remote
-      end
-    end
-
-  private
-
-    # Endow a proto object with ActiveRemote capabilities.
-    #
-    def mimic_response(response)
-      response.public_methods(false).each do |method|
-        unless respond_to?(method)
-          (class << self; self; end).class_eval do
-            define_method method do |*args|
-              response.__send__(method, *args)
-            end
-          end
-        end
       end
     end
   end
