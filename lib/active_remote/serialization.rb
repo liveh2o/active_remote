485efa0dfe0530c648582231c8d0e1c041a44c23
Adam Hutchison <adam@moneydesktop.com>
2012-09-13 11:05:48 -0600

    #serialize_records now returns nil if the last response is nil or doesn't respond to records.

--- lib/active_remote/serialization.rb
+++ lib/active_remote/serialization.rb
@@ -2,7 +2,6 @@ require 'active_remote/serializers/json'
 
 module ActiveRemote
   module Serialization
-
     def self.included(klass)
       klass.class_eval do
         include ::ActiveRemote::Serializers::JSON
@@ -10,6 +9,8 @@ module ActiveRemote
     end
 
     def serialize_records
+      return nil unless last_response.respond_to?(:records)
+
       last_response.records.map do |record|
         remote = self.class.new(record.to_hash)
         remote
