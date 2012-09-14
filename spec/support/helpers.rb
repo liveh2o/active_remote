4b1ed0b89cfb3dcc5942a1513700650a32f98bc5
Adam Hutchison <adam@moneydesktop.com>
2012-09-14 14:40:41 -0600

    Added individual helpers for resetting the DSL methods.

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -4,10 +4,34 @@ require 'support/models'
 # Reset all DSL variables so specs don't interfere with each other.
 #
 def reset_dsl_variables(klass)
-  klass.send(:instance_variable_set, :@app_name, nil)
-  klass.send(:instance_variable_set, :@auto_paging_size, nil)
-  klass.send(:instance_variable_set, :@namespace, nil)
-  klass.send(:instance_variable_set, :@publishable_attributes, nil)
-  klass.send(:instance_variable_set, :@service_class, nil)
-  klass.send(:instance_variable_set, :@service_name, nil)
+  reset_app_name(klass)
+  reset_auto_paging_size(klass)
+  reset_namespace(klass)
+  reset_publishable_attributes(klass)
+  reset_service_class(klass)
+  reset_service_name(klass)
+end
+
+def reset_app_name(klass)
+ klass.send(:instance_variable_set, :@app_name, nil)
+end
+
+def reset_auto_paging_size(klass)
+ klass.send(:instance_variable_set, :@auto_paging_size, nil)
+end
+
+def reset_namespace(klass)
+ klass.send(:instance_variable_set, :@namespace, nil)
+end
+
+def reset_publishable_attributes(klass)
+ klass.send(:instance_variable_set, :@publishable_attributes, nil)
+end
+
+def reset_service_class(klass)
+ klass.send(:instance_variable_set, :@service_class, nil)
+end
+
+def reset_service_name(klass)
+ klass.send(:instance_variable_set, :@service_name, nil)
 end
