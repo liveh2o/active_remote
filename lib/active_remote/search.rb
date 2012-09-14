16f427481fc19c622f6cea63f94c1976303a6e97
Adam Hutchison <adam@moneydesktop.com>
2012-09-13 21:37:52 -0600

    Dramatically simplified searching. Now .search always returns an array of active remote records (no more options!). Also added a new .paginated_search method that will attempt to return paginated records if will_paginate/array has been loaded.

--- lib/active_remote/search.rb
+++ lib/active_remote/search.rb
@@ -8,7 +8,30 @@ module ActiveRemote
     end
 
     module ClassMethods
-      def search(args, options = {})
+      def paginated_search(args)
+        args = _active_remote_search_args(args)
+
+        remote = self.new
+        remote._active_remote_search(args)
+        records = remote.serialize_records
+
+        if records.respond_to?(:paginate)
+          pagination_options = remote.last_response.try(:options).try(:pagination).try(:to_hash) || {}
+          records = records.paginate(pagination_options)
+        end
+
+        records
+      end
+
+      def search(args)
+        args = _active_remote_search_args(args)
+
+        remote = self.new
+        remote._active_remote_search(args)
+        remote.serialize_records
+      end
+
+      def _active_remote_search_args(args)
         unless args.is_a?(Hash)
           if args.respond_to?(:to_hash)
             args = args.to_hash
@@ -17,16 +40,14 @@ module ActiveRemote
           end
         end
 
-        remote = self.new
-        remote._active_remote_search(args)
-        options.fetch(:serialize_records, true) ? remote.serialize_records : remote
+        args
       end
     end
 
     # Search for the given resource.
     #
-    def _active_remote_search(args, options = {})
-      auto_paging = options.fetch(:auto_paging, _auto_paging?(args))
+    def _active_remote_search(args)
+      auto_paging = _auto_paging?(args)
 
       remote_records = []
       page = 0
@@ -36,7 +57,7 @@ module ActiveRemote
           page += 1
 
           if auto_paging
-            args = _auto_paging(args, _page)
+            args = _auto_paging(args, page)
           end
 
           _execute(:search, args)
@@ -54,9 +75,11 @@ module ActiveRemote
       args[:options].merge!({
         :pagination => {
           :page => page,
-          :per_page => self.class.auto_page_size
+          :per_page => self.class.auto_paging_size
         }
       })
+
+      args
     end
 
     def _auto_paging?(args)
