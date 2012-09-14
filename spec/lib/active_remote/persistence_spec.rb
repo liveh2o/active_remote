2b29efba7c1e45d8e70951a575109fc106b956a7
Adam Hutchison <adam@moneydesktop.com>
2012-09-14 17:26:21 -0600

    Specs! for the Persistence module...

--- spec/lib/active_remote/persistence_spec.rb
+++ spec/lib/active_remote/persistence_spec.rb
@@ -1,141 +1,277 @@
 require 'spec_helper'
 
 describe ActiveRemote::Persistence do
-  describe ".create" do
-    it "initializes and saves a new record"
+  subject { Tag.new }
 
-    context "when the record has errors" do
-      it "returns an unsaved record"
+  before { Tag.any_instance.stub(:last_response).and_return(HashWithIndifferentAccess.new) }
+
+  describe ".create" do
+    before { Tag.any_instance.stub(:_execute) }
+    after { Tag.any_instance.unstub(:_execute) }
+
+    it "initializes and saves a new record" do
+      Tag.any_instance.should_receive(:save)
+      Tag.create(:name => 'foo')
+    end
+
+    it "returns a new record" do
+      value = Tag.create(:name => 'foo')
+      value.should be_a(Tag)
     end
   end
 
   describe ".create!" do
-    it "initializes and saves a new record"
+    before { Tag.any_instance.stub(:_execute) }
+    after { Tag.any_instance.unstub(:_execute) }
+
+    it "initializes and saves a new record" do
+      Tag.any_instance.should_receive(:save!)
+      Tag.create!(:name => 'foo')
+    end
 
     context "when the record has errors" do
-      it "raises an exception"
+      before { Tag.any_instance.stub(:save!).and_raise(ActiveRemote::ActiveRemoteError) }
+
+      it "raises an exception" do
+        expect { Tag.create!(:name => 'foo') }.to raise_error(ActiveRemote::ActiveRemoteError)
+      end
     end
   end
 
   describe "#assign_attributes" do
-    it "updates attributes with the given hash"
+    let(:attributes) { HashWithIndifferentAccess.new(:name => 'bar') }
+
+    it "updates attributes with the given hash" do
+      subject.assign_attributes(attributes)
+      subject.attributes.should eq attributes
+    end
+
+    it "updates any attributes that have accessors defined" do
+      subject.assign_attributes(attributes)
+      subject.name.should eq attributes[:name]
+    end
   end
 
   describe "#delete" do
-    xit "deletes a remote record" do
+    before { subject.stub(:_execute) }
+    after { subject.unstub(:_execute) }
+
+    it "deletes a remote record" do
       subject.should_receive(:_execute).with(:delete, subject.attributes)
       subject.delete
+    end
+
+    context "when the record doesn't have errors" do
+      it "freezes the record" do
+        subject.delete
+        subject.frozen?.should be_true
+      end
     end
   end
 
   describe "#delete!" do
-    xit "deletes a remote record" do
+    before { subject.stub(:_execute) }
+    after { subject.unstub(:_execute) }
+
+    it "deletes a remote record" do
       subject.should_receive(:_execute).with(:delete, subject.attributes)
-      subject.delete
+      subject.delete!
     end
 
     context "when an error occurs" do
-      it "raises an exception"
+      before { subject.stub(:_execute).and_raise(ActiveRemote::ActiveRemoteError) }
+
+      it "raises an exception" do
+        expect { subject.delete! }.to raise_error(ActiveRemote::ActiveRemoteError)
+      end
     end
   end
 
   describe "#destroy" do
-    xit "destroys a remote record" do
+    before { subject.stub(:_execute) }
+    after { subject.unstub(:_execute) }
+
+    it "destroys a remote record" do
       subject.should_receive(:_execute).with(:destroy, subject.attributes)
       subject.destroy
+    end
+
+    context "when the record doesn't have errors" do
+      it "freezes the record" do
+        subject.destroy
+        subject.frozen?.should be_true
+      end
     end
   end
 
   describe "#destroy!" do
-    xit "destroys a remote record" do
+    before { subject.stub(:_execute) }
+    after { subject.unstub(:_execute) }
+
+    it "destroys a remote record" do
       subject.should_receive(:_execute).with(:destroy, subject.attributes)
-      subject.destroy
+      subject.destroy!
     end
 
     context "when an error occurs" do
-      it "raises an exception"
+      before { subject.stub(:_execute).and_raise(ActiveRemote::ActiveRemoteError) }
+
+      it "raises an exception" do
+        expect { subject.destroy! }.to raise_error(ActiveRemote::ActiveRemoteError)
+      end
     end
   end
 
   describe "#has_errors?" do
     context "when errors are not present" do
-      # its(:has_errors?) { should be_false }
+      before { subject.errors = [] }
+
+      its(:has_errors?) { should be_false }
     end
 
     context "when errors are present" do
-      # its(:has_errors?) { should be_true }
+      before { subject.errors << "Boom!" }
+
+      its(:has_errors?) { should be_true }
     end
   end
 
   describe "#new_record?" do
     context "when the record is persisted" do
-      # its(:new_record?) { should be_false }
+      subject { Tag.new(:guid => 'foo') }
+
+      its(:new_record?) { should be_false }
     end
 
     context "when the record is not persisted" do
-      # its(:new_record?) { should be_true }
+      subject { Tag.new }
+
+      its(:new_record?) { should be_true }
     end
   end
 
   describe "#persisted?" do
     context "when the record has a guid" do
-      # its(:persisted?) { should be_true }
+      subject { Tag.new(:guid => 'foo') }
+
+      its(:persisted?) { should be_true }
     end
 
     context "when the record does not have a guid" do
-      # its(:persisted?) { should be_false }
+      subject { Tag.new }
+
+      its(:persisted?) { should be_false }
     end
   end
 
   describe "#save" do
-    it "runs callbacks"
+    before { subject.stub(:_execute) }
+    after { subject.unstub(:_execute) }
 
-    context "when the record is persisted" do
-      it "updates the record"
+    it "runs callbacks" do
+      subject.should_receive(:run_callbacks).with(:save)
+      subject.save
     end
 
-    context "when the record is not persisted" do
-      it "creates the record"
+    context "when the record is new" do
+      subject { Tag.new }
+
+      it "creates the record" do
+        subject.should_receive(:_execute).with(:create, subject.attributes)
+        subject.save
+      end
+    end
+
+    context "when the record is not new" do
+      subject { Tag.new(:guid => 'foo') }
+
+      it "updates the record" do
+        subject.should_receive(:_execute).with(:update, subject.attributes)
+        subject.save
+      end
     end
 
     context "when the record is saved" do
-      it "returns true"
+      before { subject.errors = [] }
+
+      it "returns true" do
+        subject.save.should be_true
+      end
     end
 
     context "when the record is not saved" do
-      it "returns false"
+      before { subject.errors << "Boom!" }
+
+      it "returns false" do
+        subject.save.should be_false
+      end
     end
   end
 
   describe "#save!" do
-    it "saves the record"
+    before { subject.stub(:_execute) }
+    after { subject.unstub(:_execute) }
 
     context "when the record is saved" do
-      it "returns true"
+      it "returns true" do
+        subject.stub(:save).and_return(true)
+        subject.save!.should be_true
+      end
     end
 
     context "when the record is not saved" do
-      it "raises an exception"
+      it "raises an exception" do
+        subject.stub(:save).and_return(false)
+        expect { subject.save! }.to raise_error(ActiveRemote::RemoteRecordNotSaved)
+      end
     end
   end
 
   describe "#success?" do
     context "when errors are present" do
-      # its(:success?) { should be_false }
+      before { subject.errors << "Boom!" }
+
+      its(:success?) { should be_false }
     end
 
     context "when errors are not present" do
-      # its(:success?) { should be_true }
+      before { subject.errors = [] }
+
+      its(:success?) { should be_true }
     end
   end
 
   describe "#update_attributes" do
-    it "assigns new attributes"
-    it "saves the record"
+    let(:attributes) { HashWithIndifferentAccess.new(:name => 'bar') }
+
+    before { subject.stub(:save) }
+    after { subject.unstub(:save) }
+
+    it "assigns new attributes" do
+      subject.should_receive(:assign_attributes).with(attributes)
+      subject.update_attributes(attributes)
+    end
+
+    it "saves the record" do
+      subject.should_receive(:save)
+      subject.update_attributes(attributes)
+    end
   end
 
   describe "#update_attributes!" do
-    it "assigns new attributes"
-    it "saves! the record"
+    let(:attributes) { HashWithIndifferentAccess.new(:name => 'bar') }
+
+    before { subject.stub(:save!) }
+    after { subject.unstub(:save!) }
+
+    it "assigns new attributes" do
+      subject.should_receive(:assign_attributes).with(attributes)
+      subject.update_attributes!(attributes)
+    end
+
+    it "saves! the record" do
+      subject.should_receive(:save!)
+      subject.update_attributes!(attributes)
+    end
   end
 end
