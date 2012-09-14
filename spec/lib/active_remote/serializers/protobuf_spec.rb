dadb989641ee4764118055df9604c37cf40263f1
Adam Hutchison <adam@moneydesktop.com>
2012-09-14 13:38:26 -0600

    Stubbed out the the protobuf serializer specs.

--- spec/lib/active_remote/serializers/protobuf_spec.rb
+++ spec/lib/active_remote/serializers/protobuf_spec.rb
@@ -1,44 +1,58 @@
 require 'spec_helper'
 
-# TODO: Create proto messages & endpoints specifically for these tests.
-# They will be more robust if they don't rely on proto messages in Atlas.
-#
-require 'atlas/abacus/tag_service'
-
 describe ActiveRemote::Serializers::Protobuf do
-  context "when building messages" do
-    let(:records) { [ { :name => 'Foo' }, { :name => 'Bar' } ] }
-    let(:attributes) { records.first }
+  describe ".build_message" do
+    it "coerces the attribute value to a compatible type"
 
-    xit "creates a message" do
-      pending
-      described_class.service_class Abacus::TagService
-      expected_message = Abacus::Tag.new(attributes)
+    it "builds a protobuf message"
 
-      package = subject.request(:create, attributes)
-      package.should be_similar_to(expected_message)
+    context "when the message doesn't have a field matching a given attribute" do
+      it "skips the attribute"
     end
 
-    xit "creates a bulk message" do
-      pending
-      described_class.service_class Abacus::TagService
-      expected_message = Abacus::Tags.new
-
-      package = described_class.request(:create_all, records, :bulk => true)
-      package.should be_similar_to(expected_message)
+    context "when a field is repeated" do
+      it "converts the attribute to a collection"
+      it "coerces the attribute value(s) to a compatible type"
     end
 
-    xit "creates a message with nested messages" do
-      pending
-      described_class.service_class Abacus::TransactionService
-      records_hash = { :status => { :status => 7 }, :guid => 'GUID' }
+    context "when the field is an enum" do
+      it "converts the attribute into an integer"
+    end
 
-      expected_message = Abacus::Transaction.new
-      expected_message.guid = records_hash[:guid]
-      expected_message.status = Status.new(records_hash.fetch(:status)) # status => HIDDEN
+    context "when a field is a message" do
+      it "builds a new message with the attribute"
 
-      message = subject.request(:update, records_hash)
-      message.should be_similar_to(expected_message)
+      context "and the field is repeated" do
+        it "builds new messages with the attribute value(s)"
+      end
+    end
+  end
+
+  describe ".coerce" do
+    context "when field_type is :bool" do
+      context "and value is 1" do
+        it "returns true"
+      end
+
+      context "and value is 0" do
+        it "returns false"
+      end
+    end
+
+    context "when the field_type is :int32" do
+      it "returns an integer"
+    end
+
+    context "when the field_type is :int64" do
+      it "returns an integer"
+    end
+
+    context "when the field_type is :double" do
+      it "returns a float"
+    end
+
+    context "when the field_type is :string" do
+      it "returns a string"
     end
   end
 end
