d691dfd632086001a7ad6ae82fa60ac6ef536f58
Adam Hutchison <adam@moneydesktop.com>
2012-09-14 11:38:59 -0600

    Set the service class in the Tag helper model.

--- spec/support/models/tag.rb
+++ spec/support/models/tag.rb
@@ -1,6 +1,8 @@
+require 'support/protobuf/tag_service'
+
 ##
 # Define a generic class that inherits from active remote base
 #
 class Tag < ::ActiveRemote::Base
-
+  service_class ::Generic::Remote::TagService
 end
