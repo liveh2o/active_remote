c61104c79e7fb882cc5f480aac956db5a243aca8
Adam Hutchison <adam@moneydesktop.com>
2012-09-13 23:44:49 -0600

    Moved the reset DSL helper into the spec helper so other specs can use it.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,3 +39,15 @@ module Foo
     end
   end
 end
+
+##
+# Reset all DSL variables so specs don't interfere with each other.
+#
+def reset_dsl_variables(klass)
+  klass.send(:instance_variable_set, :@app_name, nil)
+  klass.send(:instance_variable_set, :@auto_paging_size, nil)
+  klass.send(:instance_variable_set, :@namespace, nil)
+  klass.send(:instance_variable_set, :@publishable_attributes, nil)
+  klass.send(:instance_variable_set, :@service_class, nil)
+  klass.send(:instance_variable_set, :@service_name, nil)
+end
