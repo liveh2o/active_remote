d18f1d8b747845ae724bd6796a3586a794f58605
Adam Hutchison <adam@moneydesktop.com>
2012-09-15 16:20:33 -0600

    Specs! for the new error serialization.

--- spec/lib/active_remote/serialization_spec.rb
+++ spec/lib/active_remote/serialization_spec.rb
@@ -1,6 +1,34 @@
 require 'spec_helper'
 
 describe ActiveRemote::Serialization do
+  describe "#add_errors_from_response" do
+    let(:error) { Generic::Error.new(:field => 'name', :message => 'Boom!') }
+    let(:last_response) {
+      tag = Generic::Remote::Tag.new
+      tag.errors << error
+      tag
+    }
+
+    subject { Tag.new }
+
+    context "when the last response has errors" do
+
+      before { subject.stub(:last_response).and_return(last_response) }
+
+      it "adds the errors to the active remote object" do
+        subject.add_errors_from_response
+        subject.errors[:name].should =~ ['Boom!']
+      end
+    end
+
+    context "when the last response doesn't respond to errors" do
+      it "doesn't add errors" do
+        subject.add_errors_from_response
+        subject.errors.empty?.should be_true
+      end
+    end
+  end
+
   describe "#serialize_records" do
     let(:last_response) {
       MessageWithOptions.new(:records => records)
