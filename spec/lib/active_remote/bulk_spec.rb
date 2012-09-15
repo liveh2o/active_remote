9aa6a50660a0247be6176f8e9bfb3742e495f45a
Adam Hutchison <adam@moneydesktop.com>
2012-09-14 18:03:38 -0600

    Specs! for the Bulk module.

--- spec/lib/active_remote/bulk_spec.rb
+++ spec/lib/active_remote/bulk_spec.rb
@@ -1,89 +1,74 @@
 require 'spec_helper'
 
 describe ActiveRemote::Bulk do
-  let(:message) do
-    double(:protos, :fields => [ double(:field, :name => :records, :repeated? => true) ], :records => [])
-  end
+  let(:records) { double(:records, :to_hash => {}) }
+  let(:serialized_records) { double(:serialized_records) }
 
   describe ".create_all" do
-    xit "creates remote records" do
-      records = []
-      described_class.any_instance.should_receive(:_execute).with(:create_all, records, :bulk => true)
-      described_class.any_instance.stub(:last_response).and_return(message)
-      described_class.create_all(records)
-    end
-  end
+    before {
+      Tag.stub(:parse_records).and_return(records)
+      Tag.any_instance.stub(:serialize_records).and_return(serialized_records)
+    }
 
-  describe ".create_all!" do
-    xit "creates remote records" do
-      records = []
-      described_class.any_instance.should_receive(:_execute).with(:create_all, records, :bulk => true)
-      described_class.any_instance.stub(:last_response).and_return(message)
-      described_class.create_all(records)
-    end
-
-    context "when any remote records have errors" do
-      it "raises an exception"
+    it "creates remote records" do
+      Tag.any_instance.should_receive(:_execute).with(:create_all, records)
+      Tag.create_all(records)
     end
   end
 
   describe ".delete_all" do
-    xit "deletes remote records" do
-      records = []
-      described_class.any_instance.should_receive(:_execute).with(:delete_all, records, :bulk => true)
-      described_class.any_instance.stub(:last_response).and_return(message)
-      described_class.delete_all(records)
-    end
-  end
+    before {
+      Tag.stub(:parse_records).and_return(records)
+      Tag.any_instance.stub(:serialize_records).and_return(serialized_records)
+    }
 
-  describe ".delete_all!" do
-    xit "deletes remote records" do
-      records = []
-      described_class.any_instance.should_receive(:_execute).with(:delete_all, records, :bulk => true)
-      described_class.any_instance.stub(:last_response).and_return(message)
-      described_class.delete_all(records)
-    end
-
-    context "when any remote records have errors" do
-      it "raises an exception"
+    it "deletes remote records" do
+      Tag.any_instance.should_receive(:_execute).with(:delete_all, records)
+      Tag.delete_all(records)
     end
   end
 
   describe ".destroy_all" do
-    it "destroys remote records"
-  end
+    before {
+      Tag.stub(:parse_records).and_return(records)
+      Tag.any_instance.stub(:serialize_records).and_return(serialized_records)
+    }
 
-  describe ".destroy_all!" do
-    it "destroy remote records"
-
-    context "when any remote records have errors" do
-      it "raises an exception"
+    it "destroys remote records" do
+      Tag.any_instance.should_receive(:_execute).with(:destroy_all, records)
+      Tag.destroy_all(records)
     end
   end
 
   describe ".parse_records" do
-    it "preps records to be built into a bulk request"
+    let(:records) { [ Hash.new ] }
+
+    it "preps records to be built into a bulk request" do
+      parsed_records = { :records => records }
+      Tag.parse_records(records).should eq parsed_records
+    end
+
+    context "when given a bulk message" do
+      let(:records) { [ tag.to_hash ] }
+      let(:tag) { Generic::Remote::Tag.new }
+      let(:tags) { Generic::Remote::Tags.new(:records => [ tag ]) }
+
+      it "preps records to be built into a bulk request" do
+        parsed_records = { :records => records }
+        Tag.parse_records(tags).should eq parsed_records
+      end
+    end
   end
 
   describe ".update_all" do
-    xit "updates remote records" do
-      records = []
-      described_class.any_instance.should_receive(:_execute).with(:update_all, records, :bulk => true)
-      described_class.any_instance.stub(:last_response).and_return(message)
-      described_class.update_all(records)
-    end
-  end
+    before {
+      Tag.stub(:parse_records).and_return(records)
+      Tag.any_instance.stub(:serialize_records).and_return(serialized_records)
+    }
 
-  describe ".update_all!" do
-    xit "updates remote records" do
-      records = []
-      described_class.any_instance.should_receive(:_execute).with(:update_all, records, :bulk => true)
-      described_class.any_instance.stub(:last_response).and_return(message)
-      described_class.update_all(records)
-    end
-
-    context "when any remote records have errors" do
-      it "raises an exception"
+    it "updates remote records" do
+      Tag.any_instance.should_receive(:_execute).with(:update_all, records)
+      Tag.update_all(records)
     end
   end
 end
