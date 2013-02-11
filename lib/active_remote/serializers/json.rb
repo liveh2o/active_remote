caf3b1f2d6de3a39301ade3df8f771d62a8778db
Adam Hutchison <adam@moneydesktop.com>
2013-02-10 18:41:12 -0700

    Updated the JSON serialization to use the new publication module methods.

--- lib/active_remote/serializers/json.rb
+++ lib/active_remote/serializers/json.rb
@@ -2,13 +2,9 @@ module ActiveRemote
   module Serializers
     module JSON
       # Returns a json representation of the whitelisted publishable attributes.
+      #
       def as_json(options = {})
-        json_attributes = _publishable_attributes || @attributes.keys
-
-        json_methods = json_attributes.reject { |attribute| @attributes.key?(attribute) }
-        json_attributes -= json_methods
-
-        default_options = { :only => json_attributes, :methods => json_methods }
+        default_options = { :only => _publishable_json_attributes, :methods => _publishable_json_methods }
         default_options.merge!(options)
 
         super(default_options)
