a56c885b5cc7cf0ceb2ba183b2db053c90d1ece5
Adam Hutchison <adam@moneydesktop.com>
2013-02-17 15:04:01 -0700

    Disabling dirty tracking while serializing records.

--- lib/active_remote/dirty.rb
+++ lib/active_remote/dirty.rb
@@ -7,6 +7,8 @@ module ActiveRemote
     def self.included(klass)
       klass.class_eval do
         include ActiveModel::Dirty
+
+        attr_accessor :_active_remote_track_changes
       end
     end
 
@@ -39,34 +41,54 @@ module ActiveRemote
       end
     end
 
-    # Override #serialize_records so that we can clear changes after
+    # Override #serialize_records so that we can skip dirty tracking while
     # initializing records returned from a search.
     #
-    def serialize_records(*)
-      if serialized_records = super
-        serialized_records.each do |record|
-          record.previous_changes.try(:clear)
-          record.changed_attributes.try(:clear)
+    def serialize_records
+      return nil unless last_response.respond_to?(:records)
+
+      last_response.records.map do |record|
+        remote = self.class.new
+        remote.skip_tracking_changes do
+          assign_attributes(record.to_hash)
         end
+        remote
       end
+    end
+
+    ##
+    # Execute the given block without tracking changes.
+    #
+    def skip_tracking_changes(&block)
+      _active_remote_track_changes = false
+      result = yield
+      _active_remote_track_changes = true
+
+      result
     end
 
     # Override #write_attribute (along with #[]=) so we can provide support for
     # ActiveModel::Dirty.
     #
     def write_attribute(name, value)
-      __send__("#{name}_will_change!") unless value == self[name]
+      __send__("#{name}_will_change!") if _active_remote_track_changes? && value != self[name]
       super
     end
     alias_method :[]=, :write_attribute
 
   private
 
+    # Wether or not changes are currently being tracked for this class.
+    #
+    def _active_remote_track_changes?
+      @_active_remote_track_changes != false
+    end
+
     # Override ActiveAttr's attribute= method so we can provide support for
     # ActiveModel::Dirty.
     #
     def attribute=(name, value)
-      __send__("#{name}_will_change!") unless value == self[name]
+      __send__("#{name}_will_change!") if _active_remote_track_changes? && value != self[name]
       super
     end
 
