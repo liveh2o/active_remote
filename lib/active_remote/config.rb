f79f6bf2ac245dc98a435688fd7608f8c98a9cf2
Brandon Dewitt <brandonsdewitt@gmail.com>
2013-11-23 13:46:57 -0700

    add structure under active_remote/core_ext and config object

---
+++ lib/active_remote/config.rb
@@ -0,0 +1,6 @@
+require 'active_support/ordered_options'
+
+module ActiveRemote
+  class Config < ::ActiveSupport::OrderedOptions
+  end
+end
---
+++ lib/active_remote/core_ext/date.rb
@@ -0,0 +1,7 @@
+Date.class_eval do
+  unless instance_methods.include?(:to_i)
+    def to_i
+      to_time.to_i
+    end
+  end
+end
