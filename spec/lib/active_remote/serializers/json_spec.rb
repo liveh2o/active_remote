d84b2dc7949304ab9be344dafeb56f644a49734a
Brandon Dewitt <brandonsdewitt@gmail.com>
2013-11-23 16:17:57 -0700

    add specs for non-rooted json

--- spec/lib/active_remote/serializers/json_spec.rb
+++ spec/lib/active_remote/serializers/json_spec.rb
@@ -7,32 +7,71 @@ describe ActiveRemote::Serializers::JSON do
 
     subject { Tag.new(attributes) }
 
-    context "when options are nil" do
-      it "substitutes an empty hash" do
-        subject.as_json(nil).should eq serializable_attributes
+    context "with roots in json" do
+      context "when options are nil" do
+        it "substitutes an empty hash" do
+          subject.as_json(nil).should eq serializable_attributes
+        end
+      end
+
+      it "accepts standard JSON options" do
+        subject.as_json(:root => false).should eq attributes.stringify_keys
+      end
+
+      context "with publishable attributes defined" do
+        let(:expected_json) { { :tag => attributes.slice(:name) }.to_json }
+
+        before { Tag.attr_publishable :name }
+        after { reset_publishable_attributes(Tag) }
+
+        it "serializes to JSON with only the publishable attributes" do
+          subject.to_json.should eq expected_json
+        end
+      end
+
+      context "without publishable attributes defined" do
+        let(:expected_json) { { :tag => attributes }.to_json }
+
+        it "serializes to JSON" do
+          subject.to_json.should eq expected_json
+        end
       end
     end
 
-    it "accepts standard JSON options" do
-      subject.as_json(:root => false).should eq attributes.stringify_keys
-    end
+    context "without roots in json" do
+      let(:serializable_attributes) { attributes.stringify_keys }
 
-    context "with publishable attributes defined" do
-      let(:expected_json) { { :tag => attributes.slice(:name) }.to_json }
-
-      before { Tag.attr_publishable :name }
-      after { reset_publishable_attributes(Tag) }
-
-      it "serializes to JSON with only the publishable attributes" do
-        subject.to_json.should eq expected_json
+      before(:each) do
+        ::ActiveRemote.config.include_root_in_json = false
       end
-    end
 
-    context "without publishable attributes defined" do
-      let(:expected_json) { { :tag => attributes }.to_json }
+      after(:each) do
+        ::ActiveRemote.config.include_root_in_json = true
+      end
 
-      it "serializes to JSON" do
-        subject.to_json.should eq expected_json
+      context "when options are nil" do
+        it "substitutes an empty hash" do
+          subject.as_json(nil).should eq serializable_attributes
+        end
+      end
+
+      context "with publishable attributes defined" do
+        let(:expected_json) { attributes.slice(:name).to_json }
+
+        before { Tag.attr_publishable :name }
+        after { reset_publishable_attributes(Tag) }
+
+        it "serializes to JSON with only the publishable attributes" do
+          subject.to_json.should eq expected_json
+        end
+      end
+
+      context "without publishable attributes defined" do
+        let(:expected_json) { attributes.to_json }
+
+        it "serializes to JSON" do
+          subject.to_json.should eq expected_json
+        end
       end
     end
   end
