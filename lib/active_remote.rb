2a0ebbf34105bf0a73d27ae8005b7f2871b742c5
Brandon Dewitt <brandonsdewitt@gmail.com>
2013-11-23 16:03:40 -0700

    require protobuf and add config object to ActiveRemote namespace method

--- lib/active_remote.rb
+++ lib/active_remote.rb
@@ -1,5 +1,7 @@
 require 'active_attr'
 require 'active_model'
+require 'protobuf'
+
 require 'active_support/core_ext/array'
 require 'active_support/core_ext/hash'
 require 'active_support/inflector'
@@ -10,6 +12,18 @@ require 'active_remote/core_ext/date'
 require 'active_remote/core_ext/integer'
 
 require 'active_remote/base'
+require 'active_remote/config'
 require 'active_remote/errors'
 
 require 'active_remote/version'
+
+module ActiveRemote
+  def self.config
+    @config ||= ::ActiveRemote::Config.new
+  end
+
+  # Initialize the config
+  config
+end
+
+require 'active_remote/railtie' if defined?(Rails)
