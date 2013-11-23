1e733ddd2045c5eb6c67f375e7bddd48cb28cdf5
Brandon Dewitt <brandonsdewitt@gmail.com>
2013-11-23 16:05:00 -0700

    add inclusion of root in json to config and default updated_at in cache_key

--- lib/active_remote/config.rb
+++ lib/active_remote/config.rb
@@ -2,5 +2,22 @@ require 'active_support/ordered_options'
 
 module ActiveRemote
   class Config < ::ActiveSupport::OrderedOptions
+
+    def initialize(options = {})
+      super
+
+      self[:default_cache_key_updated_at] = false
+      self[:include_root_in_json] = true
+    end
+
+    def default_cache_key_updated_at?
+      self[:default_cache_key_updated_at]
+    end
+
+    def include_root_in_json=(true_or_false)
+      self[:include_root_in_json] = true_or_false
+      ::ActiveRemote::Base.include_root_in_json = !!true_or_false
+    end
+
   end
 end
