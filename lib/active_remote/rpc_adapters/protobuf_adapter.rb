35bfb28d2fa55d7d7efefa2b3bd27f2d449496b0
Ryan Brock Jones <ryan.jones@mx.com>
2017-04-25 14:39:05 -0600

    Add granular protobuf errors for ActiveRemote

--- lib/active_remote/rpc_adapters/protobuf_adapter.rb
+++ lib/active_remote/rpc_adapters/protobuf_adapter.rb
@@ -22,7 +22,8 @@ module ActiveRemote
         service_class.client.__send__(rpc_method, @last_request) do |c|
           # In the event of service failure, raise the error.
           c.on_failure do |error|
-            raise ActiveRemoteError, error.message
+            protobuf_error = protobuf_error_class(error)
+            raise protobuf_error, error.message
           end
 
           # In the event of service success, assign the response.
@@ -35,6 +36,33 @@ module ActiveRemote
       end
 
       private
+
+      def protobuf_error_class(error)
+        case error.error_type
+        when ::Protobuf::Socketrpc::ErrorReason::BAD_REQUEST_DATA
+          ::ActiveRemote::BadRequestDataError
+        when ::Protobuf::Socketrpc::ErrorReason::BAD_REQUEST_PROTO
+          ::ActiveRemote::BadRequestProtoError
+        when ::Protobuf::Socketrpc::ErrorReason::SERVICE_NOT_FOUND
+          ::ActiveRemote::ServiceNotFoundError
+        when ::Protobuf::Socketrpc::ErrorReason::METHOD_NOT_FOUND
+          ::ActiveRemote::MethodNotFound
+        when ::Protobuf::Socketrpc::ErrorReason::RPC_ERROR
+          ::ActiveRemote::RpcError
+        when ::Protobuf::Socketrpc::ErrorReason::RPC_FAILED_ERROR
+          ::ActiveRemote::RpcFailedError
+        when ::Protobuf::Socketrpc::ErrorReason::INVALID_REQUEST_PROTO
+          ::ActiveRemote::InvalidRequestProtoError
+        when ::Protobuf::Socketrpc::ErrorReason::BAD_RESPONSE_PROTO
+          ::ActiveRemote::BadResponseProtoError
+        when ::Protobuf::Socketrpc::ErrorReason::UNKNOWN_HOST
+          ::ActiveRemote::UnknownHostError
+        when ::Protobuf::Socketrpc::ErrorReason::IO_ERROR
+          ::ActiveRemote::IOError
+        else
+          ActiveRemoteError
+        end
+      end
 
       # Return a protobuf request object for the given rpc request.
       #
