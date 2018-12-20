15205e71a9aa4e3c82c8afc3ac684c8e336aafe7
Brandon Dewitt <brandonsdewitt@gmail.com>
2018-12-20 15:11:24 -0700

    accomodate booleans in the boolean typecaster

--- lib/active_remote/typecasting/boolean_typecaster.rb
+++ lib/active_remote/typecasting/boolean_typecaster.rb
@@ -1,9 +1,12 @@
 module ActiveRemote
   module Typecasting
     class BooleanTypecaster
-       FALSE_VALUES = ["n", "N", "no", "No", "NO", "false", "False", "FALSE", "off", "Off", "OFF", "f", "F"]
+      BOOL_VALUES = [true, false].freeze
+      FALSE_VALUES = ["n", "N", "no", "No", "NO", "false", "False", "FALSE", "off", "Off", "OFF", "f", "F"]
 
       def self.call(value)
+        return value if BOOL_VALUES.include?(value)
+
         case value
         when *FALSE_VALUES then false
         when Numeric, /^\-?[0-9]/ then !value.to_f.zero?
