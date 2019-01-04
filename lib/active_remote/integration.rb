354a48b7ffe3a94f21a5a596d1303150de5b7a2d
adam.hutchison <adam.hutchison@mx.com>
2019-01-03 21:37:37 -0700

    Update integration module from Rails 5.2
    The integration module in Active Record adds support for integrating
    records with Action Pack (:to_param, :cache_key). Update the Active
    Remote integration to work the same way (copying much of the code from
    Active Record::Integration).

--- lib/active_remote/integration.rb
+++ lib/active_remote/integration.rb
@@ -3,48 +3,134 @@ module ActiveRemote
     extend ActiveSupport::Concern
 
     included do
-      unless singleton_methods.include?(:cache_timestamp_format)
-        ##
-        # :singleton-method:
-        # Indicates the format used to generate the timestamp format in the cache key.
-        # This is +:number+, by default.
-        #
-        def self.cache_timestamp_format
-          :number
-        end
-      end
+      ##
+      # :singleton-method:
+      # Indicates the format used to generate the timestamp in the cache key, if
+      # versioning is off. Accepts any of the symbols in <tt>Time::DATE_FORMATS</tt>.
+      #
+      # This is +:usec+, by default.
+      class_attribute :cache_timestamp_format, :instance_writer => false, :default => :usec
+
+      ##
+      # :singleton-method:
+      # Indicates whether to use a stable #cache_key method that is accompanied
+      # by a changing version in the #cache_version method.
+      #
+      # This is +false+, by default until Rails 6.0.
+      class_attribute :cache_versioning, :instance_writer => false, :default => false
     end
 
-    ##
-    # Returns a String, which can be used for constructing an URL to this
-    # object. The default implementation returns this record's guid as a String,
-    # or nil if this record's unsaved.
+    # Returns a +String+, which Action Pack uses for constructing a URL to this
+    # object. The default implementation returns this record's id as a +String+,
+    # or +nil+ if this record's unsaved.
     #
-    #   user = User.search(:name => 'Phusion')
-    #   user.to_param  # => "GUID-1"
+    # For example, suppose that you have a User model, and that you have a
+    # <tt>resources :users</tt> route. Normally, +user_path+ will
+    # construct a path with the user object's 'id' in it:
+    #
+    #   user = User.find_by(name: 'Phusion')
+    #   user_path(user)  # => "/users/1"
+    #
+    # You can override +to_param+ in your model to make +user_path+ construct
+    # a path using the user's name instead of the user's id:
+    #
+    #   class User < ActiveRecord::Base
+    #     def to_param  # overridden
+    #       name
+    #     end
+    #   end
+    #
+    #   user = User.find_by(name: 'Phusion')
+    #   user_path(user)  # => "/users/Phusion"
     #
     def to_param
-      self[:guid]&.to_s
+      key = send(primary_key)
+      key&.to_s
     end
 
-    ##
-    # Returns a cache key that can be used to identify this record.
-    #
-    # ==== Examples
+    # Returns a stable cache key that can be used to identify this record.
     #
     #   Product.new.cache_key     # => "products/new"
-    #   Person.search(:guid => "derp-5").cache_key  # => "people/derp-5-20071224150000" (include updated_at)
-    #   Product.search(:guid => "derp-5").cache_key # => "products/derp-5"
+    #   Product.find(5).cache_key # => "products/5"
+    #
+    # If ActiveRecord::Base.cache_versioning is turned off, as it was in Rails 5.1 and earlier,
+    # the cache key will also include a version.
+    #
+    #   Product.cache_versioning = false
+    #   Person.find(5).cache_key  # => "people/5-20071224150000" (updated_at available)
     #
     def cache_key
       case
       when new_record? then
-        "#{self.class.name.underscore}/new"
+        "#{model_name.cache_key}/new"
       when ::ActiveRemote.config.default_cache_key_updated_at? && (timestamp = self[:updated_at]) then
         timestamp = timestamp.utc.to_s(self.class.cache_timestamp_format)
-        "#{self.class.name.underscore}/#{self.to_param}-#{timestamp}"
+        "#{model_name.cache_key}/#{send(primary_key)}-#{timestamp}"
       else
-        "#{self.class.name.underscore}/#{self.to_param}"
+        "#{model_name.cache_key}/#{send(primary_key)}"
+      end
+    end
+
+    # Returns a cache key along with the version.
+    def cache_key_with_version
+      if (version = cache_version)
+        "#{cache_key}-#{version}"
+      else
+        cache_key
+      end
+    end
+
+    # Returns a cache version that can be used together with the cache key to form
+    # a recyclable caching scheme. By default, the #updated_at column is used for the
+    # cache_version, but this method can be overwritten to return something else.
+    #
+    # Note, this method will return nil if ActiveRecord::Base.cache_versioning is set to
+    # +false+ (which it is by default until Rails 6.0).
+    def cache_version
+      if cache_versioning && (timestamp = try(:updated_at))
+        timestamp.utc.to_s(:usec)
+      end
+    end
+
+    module ClassMethods
+      # Defines your model's +to_param+ method to generate "pretty" URLs
+      # using +method_name+, which can be any attribute or method that
+      # responds to +to_s+.
+      #
+      #   class User < ActiveRecord::Base
+      #     to_param :name
+      #   end
+      #
+      #   user = User.find_by(name: 'Fancy Pants')
+      #   user.id         # => 123
+      #   user_path(user) # => "/users/123-fancy-pants"
+      #
+      # Values longer than 20 characters will be truncated. The value
+      # is truncated word by word.
+      #
+      #   user = User.find_by(name: 'David Heinemeier Hansson')
+      #   user.id         # => 125
+      #   user_path(user) # => "/users/125-david-heinemeier"
+      #
+      # Because the generated param begins with the record's +id+, it is
+      # suitable for passing to +find+. In a controller, for example:
+      #
+      #   params[:id]               # => "123-fancy-pants"
+      #   User.find(params[:id]).id # => 123
+      def to_param(method_name = nil)
+        if method_name.nil?
+          super()
+        else
+          define_method :to_param do
+            if (default = super()) &&
+               (result = send(method_name).to_s).present? &&
+               (param = result.squish.parameterize.truncate(20, :separator => /-/, :omission => "")).present?
+              "#{default}-#{param}"
+            else
+              default
+            end
+          end
+        end
       end
     end
   end
--- spec/lib/active_remote/integration_spec.rb
+++ spec/lib/active_remote/integration_spec.rb
@@ -2,42 +2,110 @@ require "spec_helper"
 
 describe ::ActiveRemote::Integration do
   let(:guid) { "GUID-derp" }
-  subject { Tag.new(:guid => guid) }
+  let(:tag) { ::Tag.new(:guid => guid) }
+
+  subject { tag }
+
+  context ".to_param" do
+    specify { expect(::Tag).to respond_to(:to_param) }
+  end
 
   context "#to_param" do
-    # API
     specify { expect(subject).to respond_to(:to_param) }
 
     it "returns the guid if the guid is present (by default)" do
-      expect(subject.to_param).to eq(guid)
+      expect(tag.to_param).to eq(guid)
     end
   end
 
   context "#cache_key" do
-    # API
     specify { expect(subject).to respond_to(:cache_key) }
 
     it "sets 'new' as the identifier when the record has not been persisted" do
-      expect(subject).to receive(:new_record?).and_return(true)
-      expect(subject.cache_key).to match(/tag\/new/)
+      expect(tag).to receive(:new_record?).and_return(true)
+      expect(tag.cache_key).to match(/tags\/new/)
     end
 
     it "sets the cache_key to the class/guid as a default" do
-      expect(subject).to receive(:new_record?).and_return(false)
-      expect(subject.cache_key).to eq("tag/#{guid}")
+      expect(tag).to receive(:new_record?).and_return(false)
+      expect(tag.cache_key).to eq("tags/#{guid}")
     end
 
     it "adds the 'updated_at' attribute to the cache_key if updated_at is present" do
       ::ActiveRemote.config.default_cache_key_updated_at = true
-      twenty_o_one_one = subject.updated_at = DateTime.new(2001, 0o1, 0o1)
-      expect(subject).to receive(:new_record?).and_return(false)
-      expect(subject.cache_key).to eq("tag/#{guid}-#{twenty_o_one_one.to_s(:number)}")
-      subject.updated_at = nil
+      twenty_o_one_one = tag.updated_at = DateTime.new(2001, 0o1, 0o1)
+      expect(tag).to receive(:new_record?).and_return(false)
+      expect(tag.cache_key).to eq("tags/#{guid}-#{twenty_o_one_one.to_s(:usec)}")
+      tag.updated_at = nil
       ::ActiveRemote.config.default_cache_key_updated_at = false
     end
 
     it "defaults the cache updated_at to false" do
       expect(::ActiveRemote.config.default_cache_key_updated_at?).to be_falsey
+    end
+  end
+
+  describe "#cache_key_with_version" do
+    let(:tag) { ::Tag.new(:guid => guid, :updated_at => ::DateTime.current) }
+
+    specify { expect(subject).to respond_to(:cache_key_with_version) }
+
+    context "when cache versioning is enabled" do
+      around do |example|
+        cache_versioning_value = ::Tag.cache_versioning
+        ::Tag.cache_versioning = true
+        example.run
+        ::Tag.cache_versioning = cache_versioning_value
+      end
+
+      it "returns a cache key with the version" do
+        expect(tag.cache_version).to be_present
+        expect(tag.cache_key_with_version).to eq("#{tag.cache_key}-#{tag.cache_version}")
+      end
+    end
+
+    context "when cache versioning is not enabled" do
+      around do |example|
+        cache_versioning_value = ::Tag.cache_versioning
+        ::Tag.cache_versioning = false
+        example.run
+        ::Tag.cache_versioning = cache_versioning_value
+      end
+
+      it "returns a cache key without the version" do
+        expect(tag.cache_key_with_version).to eq(tag.cache_key)
+      end
+    end
+  end
+
+  describe "#cache_version" do
+    specify { expect(subject).to respond_to(:cache_version) }
+
+    context "when cache versioning is enabled" do
+      around do |example|
+        cache_versioning_value = ::Tag.cache_versioning
+        ::Tag.cache_versioning = true
+        example.run
+        ::Tag.cache_versioning = cache_versioning_value
+      end
+
+      it "returns a cache version" do
+        tag.updated_at = DateTime.new(2001, 0o1, 0o1)
+        expect(tag.cache_version).to eq(tag.updated_at.utc.to_s(:usec))
+      end
+    end
+
+    context "when cache versioning is not enabled" do
+      around do |example|
+        cache_versioning_value = ::Tag.cache_versioning
+        ::Tag.cache_versioning = false
+        example.run
+        ::Tag.cache_versioning = cache_versioning_value
+      end
+
+      it "returns nil" do
+        expect(tag.cache_version).to be_nil
+      end
     end
   end
 end
