48cb1aae693d04d7eb4d92ea3427e0bbfa9c5f93
adam.hutchison <adam.hutchison@mx.com>
2019-01-09 13:12:48 -0700

    Use type casting from Active Model
    Active Model 5.0 includes type casting from Active Record, and a type
    registry to register and use custom types. Bring Active Remote inline
    with Active Record by replacing the custom type casters extracted from
    ActiveAttr with the type casting from Active Model.
    
    This change brings a significant speed boost (30% faster initialization,
    and 70% faster instantiation), but also removes the old method of
    definition custom types. Instead of a providing a class that responds
    to `call`, custom types must be
    [registered](https://github.com/rails/rails/blob/5-0-stable/activemodel/lib/active_model/type.rb),
    and respond to `deserialize` (value for RPC), and `cast` (value from
    user). See
    [ActiveModel::Type::Value](https://github.com/rails/rails/blob/5-0-stable/activemodel/lib/active_model/type/value.rb)
    for details.

--- lib/active_remote.rb
+++ lib/active_remote.rb
@@ -1,5 +1,6 @@
 require 'active_model'
 require 'active_support'
+require 'active_support/core_ext'
 require 'protobuf'
 
 require 'active_remote/base'
--- lib/active_remote/typecasting/float_typecaster.rb
+++
@@ -1,9 +0,0 @@
-module ActiveRemote
-  module Typecasting
-    class FloatTypecaster
-      def self.call(value)
-        value.to_f if value.respond_to?(:to_f)
-      end
-    end
-  end
-end
