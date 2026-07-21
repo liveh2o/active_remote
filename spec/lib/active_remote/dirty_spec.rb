a0b95cfd107413beef60314f985688494f6cd9a5
John Bolliger <johnbolliger@gmail.com>
2026-07-21 13:30:12 -0700

    more specs

--- spec/lib/active_remote/dirty_spec.rb
+++ spec/lib/active_remote/dirty_spec.rb
@@ -4,10 +4,7 @@ RSpec.describe ActiveRemote::Dirty do
   context "#attribute=" do
     subject(:post) { Post.new(name: "foo") }
 
-    before do
-      post.changes_applied
-      post.clear_changes_information
-    end
+    before { reset_changes(post) }
 
     context "when the value changes" do
       it "tracks changes" do
@@ -27,10 +24,7 @@ RSpec.describe ActiveRemote::Dirty do
   describe "#[]=" do
     subject(:post) { Post.new(name: "foo") }
 
-    before do
-      subject.changes_applied
-      subject.clear_changes_information
-    end
+    before { reset_changes(post) }
 
     context "when the value changes" do
       it "tracks changes" do
@@ -92,6 +86,46 @@ RSpec.describe ActiveRemote::Dirty do
       post.save!
       expect(post.previous_changes).to eq(changes)
       expect(post.changes).to be_empty
+    end
+  end
+
+  # Dirty tracking compares CAST values, not the raw assigned value. This is only
+  # observable on typed attributes, so the string-only specs above don't cover it.
+  describe "cast-aware change tracking" do
+    subject(:author) { Author.new(age: 42, writes_fiction: true) }
+
+    before { reset_changes(author) }
+
+    context "when a raw value casts equal to the current value" do
+      it "does not mark an integer attribute changed" do
+        author.age = "42" # String that casts to the stored Integer 42
+        expect(author.age_changed?).to be(false)
+      end
+
+      it "does not mark a boolean attribute changed" do
+        author.writes_fiction = "t" # casts to the stored true
+        expect(author.writes_fiction_changed?).to be(false)
+      end
+
+      it "is cast-aware through #[]= as well" do
+        author[:age] = "42"
+        expect(author.age_changed?).to be(false)
+      end
+    end
+
+    context "when a raw value casts to a different value" do
+      it "marks the attribute changed, casting both endpoints" do
+        author.age = "43"
+        expect(author.age_change).to eq([42, 43])
+      end
+    end
+  end
+
+  describe "change tracking on a new record" do
+    it "reports assigned attributes as changes, cast to their type" do
+      author = Author.new(age: "5")
+
+      expect(author.changes).to eq("age" => [nil, 5])
     end
   end
 end
